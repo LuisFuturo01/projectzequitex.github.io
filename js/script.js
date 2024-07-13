2923de0cbf4c6512c3908d59e93bb700024f1a20
LuisFuturo <luis.futuro.01@gmail.com>
2024-07-12 22:30:38 -0400

    acabando el section llamativo, con interactividad

--- js/script.js
+++ js/script.js
@@ -41,17 +41,53 @@ if(!isTouch){
         });
     };
     cursorPersonality();
-    documen.body.classList.add(hasCustomCursosClass);
+    document.body.classList.add(hasCustomCursosClass);
 }
 //Fin creando cursor personalizado
 
 //eliminando el animation-delay, de sidebar, y de open-button
-document.addEventListener('DOMContentLoaded', function() {
-    setTimeout(function() {
-        var elements = document.querySelectorAll('.open-button, .sidebar');
-        elements.forEach(function(element) {
-            element.style.setProperty('animation-delay', '0s');
-        });
-    }, 430);
-});
+const delaySidebar=()=>{
+    document.addEventListener('DOMContentLoaded', function() {
+        setTimeout(function() {
+            var elements = document.querySelectorAll('.sidebar__form, .sidebar');
+            elements.forEach(function(element) {
+                element.style.setProperty('animation-delay', '0s');
+            });
+        }, 5);
+    });
+};
+delaySidebar();
 //Fin de eliminando delay del sidebar-open
+//añadiendo triángulos locos al section principal
+const groupTriangles = document.getElementById("group-triangles");
+
+const addTriangles = (numTriangles) => {
+    for (let i = 0; i < numTriangles; i++) {
+        const transformX = Math.random() * 93;
+        const transformY = Math.random() * 7.2;
+        const rotation = calculateRotation(transformX, transformY);
+        const codePathTriangles = `
+            <g style="transform: translate(${transformX}%, ${transformY}vh) rotate(${rotation}deg);" class="g-triangle">
+                <path class="path-triangle" d="M0 0 8 1 2 7 3 3 0 0" />
+            </g>
+        `;
+        groupTriangles.insertAdjacentHTML('beforeend', codePathTriangles);
+    };
+};
+const updateTriangles = () => {
+    const triangles = document.querySelectorAll('.g-triangle');
+    triangles.forEach(triangle => {
+        const transformX = Math.random() * 93;
+        const transformY = Math.random() * 7.2;
+        const rotation = calculateRotation(transformX, transformY);
+        triangle.style.transform = `translate(${transformX}%, ${transformY}vh) rotate(${rotation}deg)`;
+    });
+};
+const calculateRotation = (x, y) => {
+    const angle = Math.atan2(y - 50, x - 50) * (180 / Math.PI);
+    const speedFactor = 6;
+    return angle * speedFactor;
+};
+setInterval(updateTriangles, 5000);
+addTriangles(20);
+//acabando el efecto triángulos locos
